908b9cfe17a06e7436a6a48fb51aeca6af3f0d39
Robin He <31844669+robinhetw@users.noreply.github.com>
2018-03-12 00:14:51 +0800

    Add files via upload

---
+++ main.bundle.js
@@ -0,0 +1,297 @@
+webpackJsonp(["main"],{
+
+/***/ "./src/$$_lazy_route_resource lazy recursive":
+/***/ (function(module, exports) {
+
+function webpackEmptyAsyncContext(req) {
+	// Here Promise.resolve().then() is used instead of new Promise() to prevent
+	// uncatched exception popping up in devtools
+	return Promise.resolve().then(function() {
+		throw new Error("Cannot find module '" + req + "'.");
+	});
+}
+webpackEmptyAsyncContext.keys = function() { return []; };
+webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
+module.exports = webpackEmptyAsyncContext;
+webpackEmptyAsyncContext.id = "./src/$$_lazy_route_resource lazy recursive";
+
+/***/ }),
+
+/***/ "./src/app/app.component.css":
+/***/ (function(module, exports) {
+
+module.exports = "\r\n"
+
+/***/ }),
+
+/***/ "./src/app/app.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<section class=\"col-lg-12 col-md-12 col-12\">\n  <div>\n    <div>\n      <app-todo></app-todo>\n    </div>\n  </div>\n</section>"
+
+/***/ }),
+
+/***/ "./src/app/app.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var AppComponent = /** @class */ (function () {
+    function AppComponent() {
+        this.title = 'app';
+    }
+    AppComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
+            selector: 'app-root',
+            template: __webpack_require__("./src/app/app.component.html"),
+            styles: [__webpack_require__("./src/app/app.component.css")]
+        })
+    ], AppComponent);
+    return AppComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/app.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__("./node_modules/@angular/platform-browser/esm5/platform-browser.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_angularfire2__ = __webpack_require__("./node_modules/angularfire2/index.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3_angularfire2_database__ = __webpack_require__("./node_modules/angularfire2/database/index.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__app_component__ = __webpack_require__("./src/app/app.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__environments_environment__ = __webpack_require__("./src/environments/environment.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__todo_todo_component__ = __webpack_require__("./src/app/todo/todo.component.ts");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+
+
+
+var AppModule = /** @class */ (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["E" /* NgModule */])({
+            declarations: [
+                __WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */],
+                __WEBPACK_IMPORTED_MODULE_6__todo_todo_component__["a" /* TodoComponent */]
+            ],
+            imports: [
+                __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
+                __WEBPACK_IMPORTED_MODULE_2_angularfire2__["a" /* AngularFireModule */].initializeApp(__WEBPACK_IMPORTED_MODULE_5__environments_environment__["a" /* environment */].firebase),
+                __WEBPACK_IMPORTED_MODULE_3_angularfire2_database__["b" /* AngularFireDatabaseModule */]
+            ],
+            providers: [],
+            bootstrap: [__WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */]]
+        })
+    ], AppModule);
+    return AppModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/todo/shared/todo.service.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return TodoService; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_angularfire2_database__ = __webpack_require__("./node_modules/angularfire2/database/index.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+var TodoService = /** @class */ (function () {
+    function TodoService(firebasedb) {
+        this.firebasedb = firebasedb;
+    }
+    TodoService.prototype.getToDoList = function () {
+        this.toDoList = this.firebasedb.list('titles');
+        return this.toDoList;
+    };
+    TodoService.prototype.addTitle = function (title) {
+        this.toDoList.push({
+            title: title,
+            isChecked: false
+        });
+    };
+    TodoService.prototype.checkOrUnCheckTitle = function ($key, flag) {
+        this.toDoList.update($key, { isChecked: flag });
+    };
+    TodoService.prototype.removeTitle = function ($key) {
+        this.toDoList.remove($key);
+    };
+    TodoService = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["w" /* Injectable */])(),
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_angularfire2_database__["a" /* AngularFireDatabase */]])
+    ], TodoService);
+    return TodoService;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/todo/todo.component.css":
+/***/ (function(module, exports) {
+
+module.exports = "\r\n"
+
+/***/ }),
+
+/***/ "./src/app/todo/todo.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<div id=\"myDIV\" class=\"header\">\n  <h2 style=\"margin:5px\">My To Do List</h2>\n  <div class=\"input-group\">\n    <input type=\"text\" class=\"form-control\" placeholder=\"Title\" #itemTitle>\n    <div class=\"input-group-addon hover-cursor\" (click)=\"onAdd(itemTitle)\">\n      <span class=\"addBtn\">Add</span>\n    </div>\n  </div>\n</div>\n\n\n\n\n<!--<div class=\" title-top\">\n  <h4 class=\"text-center\">My To Do List</h4>\n\n  <div class=\"input-group\">\n    <input type=\"text\" class=\"form-control\" placeholder=\"Title\" #itemTitle>\n    <div class=\"input-group-addon hover-cursor\" (click)=\"onAdd(itemTitle)\">\n      <button class=\"addBtn\">Add</button>-->\n\n<!--<i class=\"fa fa-plus-circle fa-2x\"></i>-->\n<!--</div>\n  </div>\n</div>-->\n\n\n<div>\n  <ul class=\"list-group\">\n    <li class=\"list-group-item \" *ngFor=\"let item of toDoListArray\">\n      <span class=\"hover-cursor checked-tag\" [class.text-success]=\"item.isChecked\" (click)=\"alterCheck(item.$key,item.isChecked)\">\n        <i class=\"fa fa-lg\" [ngClass]=\"item.isChecked?'fa-check':'fa-check-circle'\"></i>\n      \n      {{item.title}}</span>\n      <span class=\"hover-cursor text-danger pull-right\" (click)=\"onDelete(item.$key)\">\n        <i class=\"fa fa-times fa-lg close\"></i>\n      </span>\n\n    </li>\n  </ul>\n</div>"
+
+/***/ }),
+
+/***/ "./src/app/todo/todo.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return TodoComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__shared_todo_service__ = __webpack_require__("./src/app/todo/shared/todo.service.ts");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+var TodoComponent = /** @class */ (function () {
+    function TodoComponent(toDoService) {
+        this.toDoService = toDoService;
+    }
+    TodoComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        this.toDoService.getToDoList().snapshotChanges()
+            .subscribe(function (item) {
+            _this.toDoListArray = [];
+            item.forEach(function (element) {
+                var x = element.payload.toJSON();
+                x["$key"] = element.key;
+                _this.toDoListArray.push(x);
+            });
+            //sort array isChecked false ->true
+            _this.toDoListArray.sort(function (a, b) {
+                return a.isChecked - b.isChecked;
+            });
+        });
+    };
+    TodoComponent.prototype.onAdd = function (itemTitle) {
+        this.toDoService.addTitle(itemTitle.value);
+        itemTitle.value = null;
+    };
+    TodoComponent.prototype.alterCheck = function ($key, isChecked) {
+        this.toDoService.checkOrUnCheckTitle($key, !isChecked);
+    };
+    TodoComponent.prototype.onDelete = function ($key) {
+        this.toDoService.removeTitle($key);
+    };
+    TodoComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
+            selector: 'app-todo',
+            template: __webpack_require__("./src/app/todo/todo.component.html"),
+            styles: [__webpack_require__("./src/app/todo/todo.component.css")],
+            providers: [__WEBPACK_IMPORTED_MODULE_1__shared_todo_service__["a" /* TodoService */]]
+        }),
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1__shared_todo_service__["a" /* TodoService */]])
+    ], TodoComponent);
+    return TodoComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/environments/environment.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+var environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDzaSpspSQrlvkIQH46wqxNzCY-NhTG-WA",
+        authDomain: "todolistapp-d0cc9.firebaseapp.com",
+        databaseURL: "https://todolistapp-d0cc9.firebaseio.com",
+        projectId: "todolistapp-d0cc9",
+        storageBucket: "todolistapp-d0cc9.appspot.com",
+        messagingSenderId: "460690017136"
+    }
+};
+
+
+/***/ }),
+
+/***/ "./src/main.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("./node_modules/@angular/core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__ = __webpack_require__("./node_modules/@angular/platform-browser-dynamic/esm5/platform-browser-dynamic.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_app_module__ = __webpack_require__("./src/app/app.module.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__environments_environment__ = __webpack_require__("./src/environments/environment.ts");
+
+
+
+
+if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_5" /* enableProdMode */])();
+}
+Object(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */])
+    .catch(function (err) { return console.log(err); });
+
+
+/***/ }),
+
+/***/ 0:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__("./src/main.ts");
+
+
+/***/ })
+
+},[0]);
+//# sourceMappingURL=main.bundle.js.map
